71c4ad0630d7b3c3d6b2fe8628c9c6dc2d31c0f8
Barak Dafna <barakdf@gmail.com>
2024-03-09 15:54:11 +0200

    added validation for subject id on load dummy data for tests

--- Assets/_Scripts/UI/CheckSubjectID.cs
+++ Assets/_Scripts/UI/CheckSubjectID.cs
@@ -1,31 +1,54 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class CheckSubjectID : MonoBehaviour
 {
-    [SerializeField] InputField subjectID;
-    [SerializeField] TextMeshPro errorText;
-
+    [SerializeField] TMP_InputField subjectID;
+    [SerializeField] TextMeshProUGUI errorText;
+    [SerializeField] private string errorMessage = "Subject ID doesn't exist";
     [SerializeField] private GameObject form;
-// check if the subject ID is valid, should be 4 digits
-    public void CheckID()
-    {
-        if (subjectID.text.Length != 4)
-        {
-            errorText.text = "Subject ID should be 4 digits";
+
+    private bool idIsValid;
+
+    public void ValidateID() {
+        if (subjectID.text.Length == 4) {
+            idIsValid = true;
+            // check for subject folder existence
         }
-        else
-        {
-            errorText.text = "";
+        else {
+            idIsValid = false;
         }
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-        
+    // check if the subject ID is valid, should be 4 digits
+    public void CheckID() {
+        if (idIsValid && SubjectExists()) {
+            errorText.gameObject.SetActive(false);
+            form.SetActive(true);
+        }
+        else {
+            errorText.gameObject.SetActive(true);
+        }
+    }
+    
+    private bool SubjectExists() {
+        string patientID = subjectID.text;
+        string subjectFolderPath = Path.Combine(Application.dataPath, "Model/Subjects", patientID);
+        return Directory.Exists(subjectFolderPath);
+    }
+
+    private void OnEnable() {
+        errorText.text = errorMessage;
+        errorText.gameObject.SetActive(false);
+    }
+
+    private void OnDisable() {
+        subjectID.text = String.Empty;
+        errorText.gameObject.SetActive(false);
+        form.SetActive(false);
     }
 
     // Update is called once per frame
@@ -33,4 +56,8 @@ public class CheckSubjectID : MonoBehaviour
     {
         
     }
+    
+    
 }
+
+
